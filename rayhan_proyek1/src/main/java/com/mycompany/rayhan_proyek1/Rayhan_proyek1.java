f14dd42dedef607fa625bbf46bf4776de0eb5376
rayhanadityaputra <rayhan.aditya.putra@students.untidar.ac.id>
2026-08-26 14:22:10 +0700

    Commit pertama: Menambahkan tugas proyek 1

---
+++ rayhan_proyek1/src/main/java/com/mycompany/rayhan_proyek1/Rayhan_proyek1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rayhan_proyek1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Rayhan_proyek1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Rayhan Aditya Putra");
+        System.out.println("NPM    : 2505060005");
+        System.out.println("Alamat : Kota Magelang");  
+        System.out.println("No_HP  : 089626783076");
+    }
+}
+
